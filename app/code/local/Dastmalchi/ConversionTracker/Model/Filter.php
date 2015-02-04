213b149ba0a6c375965fa44571289df86ebf4d08
Daniel Smith <daniel@dastmalchi.com>
2015-02-03 16:49:48 -0800

    Adds getAll method

--- app/code/local/Dastmalchi/ConversionTracker/Model/Filter.php
+++ app/code/local/Dastmalchi/ConversionTracker/Model/Filter.php
@@ -12,6 +12,42 @@
 class Dastmalchi_Conversiontracker_Model_Filter extends Varien_Filter_Template
 {
     /**
+     * Override _getVariable() to provide a workaround for {{array order.getAllItems()}} and {{array order.getAllVisibleItems()}}
+     */
+    protected function _overrideGetVariable($value, $default='{no_value_defined}')
+    {
+        /**
+         * Workaround for getAllItems()
+         */
+        if ($value == " order.getAllItems()") {
+            $_result = array();
+            $_products = $this->_getVariable($value, '');
+            foreach($_products as $product) {
+                $pid = $product->getSku();
+                $product = Mage::getModel('catalog/product')->loadByAttribute('sku', $pid);
+                array_push($_result, $product->getData());
+            }
+            return $_result;
+        }
+
+        /**
+         * Workaround for getAllVisibleItems()
+         */
+        if ($value == " order.getAllVisibleItems()") {
+            $_result = array();
+            $_products = $this->_getVariable($value, '');
+            foreach($_products as $product) {
+                $pid = $product->getSku();
+                $product = Mage::getModel('catalog/product')->loadByAttribute('sku', $pid);
+                array_push($_result, $product->getData());
+            }
+            return $_result;
+        }
+
+        return $this->_getVariable($value, '');
+    }
+
+    /**
      * Enables Varien Filter to display arrays as JSON
      */
     public function arrayDirective($construction)
@@ -21,7 +57,7 @@ class Dastmalchi_Conversiontracker_Model_Filter extends Varien_Filter_Template
             return $construction[0];
         }
 
-        $replacedValue = json_encode($this->_getVariable($construction[2], ''));
+        $replacedValue = json_encode($this->_overrideGetVariable($construction[2], ''));
         return $replacedValue;
     }
 }
